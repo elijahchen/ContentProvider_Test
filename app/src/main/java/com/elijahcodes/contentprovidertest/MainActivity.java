7845daa2cb572515b2a2665a0b46af2ba647a6d4
elijahchen <code.elijahchen@gmail.com>
2016-12-01 20:16:42 -0500

    - Added ContactsContract.Contacts!!

--- app/src/main/java/com/elijahcodes/contentprovidertest/MainActivity.java
+++ app/src/main/java/com/elijahcodes/contentprovidertest/MainActivity.java
@@ -1,15 +1,21 @@
 package com.elijahcodes.contentprovidertest;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private ListView contactNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        contactNames = (ListView) findViewById(R.id.contact_names);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Log.d(TAG, "fab onClick: Starts");
+                String[] projection = {
+                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+                };
+                ContentResolver contentResolver = getContentResolver();
+                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY);
             }
         });
     }
